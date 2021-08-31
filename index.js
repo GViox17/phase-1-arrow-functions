b952bac4bf551921b2d8e5e40d2e3541663cf594
Gabriel Viox <gviox17@gmail.com>
2021-08-31 17:48:54 -0500

    commit tree 92b58ca1d916990781e6f2f996507cc0fba7cc95

---
+++ index.js
@@ -0,0 +1,12 @@
+const num1 = Math.abs(2000);
+const num2 = Math.abs(100);
+const a = 3;
+const b = 4;
+const divide = function() {
+    return num1 / num2;
+}
+divide();
+
+const square = (x) => x * x;
+
+const add = (a,b) => a + b;
